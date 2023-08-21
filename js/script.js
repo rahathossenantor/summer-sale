3227df704462d6aeb06fbf570b4e56a8d5135e3f
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-08-21 14:02:47 +0600

    event handler added on coupon apply button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.clear();
+// sort version of document.getElementById();
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+const couponApplyBtn = getElement("coupon-apply-btn");
+const couponInput = getElement("coupon-field");
+const totalPrice = parseFloat(getElement("total-price").innerText);
+
+// event handler on coupon input field
+couponInput.addEventListener("keyup", function(event) {
+    const couponInputValue = event.target.value;
+    
+    if (couponInputValue === "SELL200" && totalPrice > 0) {
+        couponApplyBtn.disabled = false;
+        return;
+    } else {
+        couponApplyBtn.disabled = true;
+        return;
+    }
+});
+
+// event handler on coupon submit button
+couponApplyBtn.addEventListener("click", function() {
+    couponInput.value = "";
+    couponApplyBtn.disabled = true;
+    return;
+});
